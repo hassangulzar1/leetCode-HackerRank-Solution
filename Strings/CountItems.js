7f17ddf9d953713f86b5c6e08f109d90c4cd6b46
hassangulzar1 <hassangulzar121@gmail.com>
2023-07-19 18:20:21 +0500

    count items

---
+++ Strings/CountItems.js
@@ -0,0 +1,18 @@
+let items = [
+  ["phone", "blue", "pixel"],
+  ["computer", "silver", "lenovo"],
+  ["phone", "gold", "iphone"],
+];
+let ruleKey = "color";
+let ruleValue = "silver";
+let countMatches = (items, ruleKey, ruleValue) => {
+  let index = ["type", "color", "name"].indexOf(ruleKey);
+  let count = 0;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i][index] === ruleValue) {
+      count += 1;
+    }
+  }
+  return count;
+};
+console.log(countMatches(items, ruleKey, ruleValue));
