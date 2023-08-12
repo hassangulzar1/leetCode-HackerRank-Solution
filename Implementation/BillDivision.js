aa2f37cff1613f2e8b8b8853d16e04e73ed9bab7
hassangulzar1 <hassangulzar121@gmail.com>
2023-08-12 19:50:45 +0500

    billdivision

---
+++ Implementation/BillDivision.js
@@ -0,0 +1,14 @@
+let bill = [3, 10, 2, 9];
+let k = 1;
+let b = 7;
+
+let bonAppetit = (bill, k, b) => {
+  let sum = bill.reduce((prev, curr) => prev + curr, 0);
+  let collectedBill = (sum - bill[k]) / 2;
+  if (collectedBill === b) {
+    console.log("Bon Appetit");
+  } else {
+    console.log(b - collectedBill);
+  }
+};
+console.log(bonAppetit(bill, k, b));
