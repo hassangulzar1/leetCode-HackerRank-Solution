c6e86c9110ee997cc44084e5b7ea5fb4e382707f
hassangulzar1 <hassangulzar121@gmail.com>
2023-08-22 21:52:25 +0500

    bill division

--- Implementation/BillDivision.js
+++ Implementation/BillDivision.js
@@ -1,7 +1,6 @@
 let bill = [3, 10, 2, 9];
 let k = 1;
 let b = 7;
-
 let bonAppetit = (bill, k, b) => {
   let sum = bill.reduce((prev, curr) => prev + curr, 0);
   let collectedBill = (sum - bill[k]) / 2;
